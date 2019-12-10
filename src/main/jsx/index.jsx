b0141d3841cd1a8815a6d42d4fc4a14795e6ee91
KimJongHyeok2 <proje_ct@naver.com>
2019-12-11 01:04:55 +0900

    ReInitial Commit

---
+++ src/main/jsx/index.jsx
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import ReactDOM from 'react-dom';
+
+class Index extends PureComponent {
+    render() {
+        return <div>Index</div>;
+    }
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
